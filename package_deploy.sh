d246a3dda7ec75c8d0a08a9990d21e080e358c4d
matth <matt@matthaynes.net>
2009-05-11 21:29:19 +0100

    Edit package deploy, more sensible now

---
+++ package_deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "Usage: `basename $0` [incoming_dir] [tmp_dir]";
+	echo
+	echo "Untars any *.tar.gz files it finds in incoming_dir to tmp_dir, looks for deploy.sh in root of tar and runs it"
+	echo
+else 
+	INCOMING_DIR=`dirname $1`/`basename $1`
+	TMP_DIR=`dirname $2`/`basename $2`
+
+	for f in $( ls $INCOMING_DIR/*.tar.gz ); do
+		mv $f $TMP_DIR
+		cd $TMP_DIR
+		tar -xvzf `basename $f`
+		cd -
+		source $TMP_DIR/deploy.sh
+		rm -rdf $TMP_DIR/*
+	done	
+fi
